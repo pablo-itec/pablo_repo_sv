1702d9d1e981e45c65426e2951d9d2a5b75a0c45
pablo-itec <p.abataneo@itecriocuarto.org.ar>
2025-08-25 15:24:27 -0300

    Initial commit

---
+++ ej2_pablo_abataneo.js
@@ -0,0 +1,20 @@
+var precioPrimerProducto = 50;
+var precioSegundoProducto = 30;
+var esEstudiante = false;
+
+var total = precioPrimerProducto + precioSegundoProducto;
+
+console.log("precio primer producto: " + precioPrimerProducto);
+console.log("precio segundo producto: " + precioSegundoProducto);
+console.log("total sin descuento: " + total);
+
+if (esEstudiante) {
+  console.log("sos estudiate!!! entonces tenes 10% de descuento");
+  var descuento = total * 0.10;
+  console.log("Descuento: " + descuento);
+  var totalConDescuento = total - descuento;
+  console.log("total con descuento: " + totalConDescuento);
+} else {
+  console.log("no sos estudiante, no hay descuento");
+  console.log("total a pagar: " + total);
+}
